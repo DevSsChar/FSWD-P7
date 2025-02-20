f10fa03d6ab621c568bb41f436842cee7276ed6a
Dev <23it116@charusat.edu.in>
2025-02-20 14:32:45 +0530

    no need of node file

--- node.js
+++
@@ -1,6 +0,0 @@
-// Write a connection script in Node.js using Mongoose to connect to MongoDB.
-// Define a User schema with fields like name, email, and age.
-// Create a simple script to insert a user into the database and log the results in the console.
-// Fetch all users from the database and display them in the console
-// const User=require('./mongo.js')
-// console.log("user")
